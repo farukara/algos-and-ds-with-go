e4dddc7a1bf2f92e0b655a44b1dfbdba1b4d0546
farukara <69348522+farukara@users.noreply.github.com>
2021-11-18 17:35:48 +0300

    modded addingWithoutAPlus

--- addingWithoutAPlus.go
+++ addingWithoutAPlus.go
@@ -1,22 +1,24 @@
 package main
-//Adding 2 numbers without using "+" operator
+//Adding 2 POSITIVE numbers without using "+" operator
 
 import (
     "fmt"
 )
 
-func AddingWithoutPlus (a, b int) int {
+func AddingWithoutPlus (a, b uint) uint {
     carry := (a & b) << 1
     total := a^b
+    var temp uint
     for carry > 0 {
+        temp  = total&carry << 1
         total = total^carry
-        carry = (total ^ carry) << 1
+        carry = temp
     }
     return total
 }
 
 func main() {
-    a := 500
-    b := 10
+    const a uint = 12
+    const b uint = 500
     fmt.Println(AddingWithoutPlus(a, b))
 }
